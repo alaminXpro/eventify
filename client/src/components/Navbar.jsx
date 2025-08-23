12c64a3555b474237eb990b4ec809fa3f65da3be
jnkarim <julkernkarim@gmail.com>
2025-08-24 03:21:55 +0600

    Fix Event Creation UI.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -175,7 +175,7 @@ export default function Navbar() {
               href="/profile"
               className="px-3 py-2 rounded-md text-gray-200 hover:text-white hover:bg-white/10 transition"
             >
-              Profile
+              Profile Page
             </a>
           </li>
         </ul>
